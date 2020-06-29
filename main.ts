1b36aea82ffa2b0fce28666eee162d8914172d59
sf-sg <62641897+sf-sg@users.noreply.github.com>
2020-06-29 18:19:03 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,43 @@
 let item = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
 basic.forever(function () {
     for (let index = 0; index < 10; index++) {
-        for (let index = 0; index <= 2; index++) {
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Red))
+        for (let index2 = 0; index2 <= 2; index2++) {
+            item.range(index2, 1).showColor(neopixel.colors(NeoPixelColors.Red))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Orange))
+            item.range(index2, 1).showColor(neopixel.colors(NeoPixelColors.Orange))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Yellow))
+            item.range(index2, 1).showColor(neopixel.colors(NeoPixelColors.Yellow))
             basic.pause(100)
         }
     }
     for (let index = 0; index < 10; index++) {
-        for (let index = 0; index <= 2; index++) {
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Red))
+        for (let index4 = 0; index4 <= 2; index4++) {
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Red))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Orange))
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Orange))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Yellow))
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Yellow))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Green))
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Green))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Blue))
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Blue))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Violet))
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Violet))
             item.shift(1)
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Purple))
+            item.range(index4, 1).showColor(neopixel.colors(NeoPixelColors.Purple))
             basic.pause(100)
         }
     }
     for (let index = 0; index < 20; index++) {
-        for (let index = 0; index <= 2; index++) {
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Blue))
+        for (let index6 = 0; index6 <= 2; index6++) {
+            item.range(index6, 1).showColor(neopixel.colors(NeoPixelColors.Blue))
             item.shift(1)
             basic.pause(30)
         }
     }
     for (let index = 0; index < 30; index++) {
-        for (let index = 0; index <= 2; index++) {
-            item.range(index, 1).showColor(neopixel.colors(NeoPixelColors.Purple))
+        for (let index8 = 0; index8 <= 2; index8++) {
+            item.range(index8, 1).showColor(neopixel.colors(NeoPixelColors.Purple))
             item.shift(1)
             basic.pause(20)
         }
